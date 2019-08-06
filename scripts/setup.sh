b40ce15b9036b282f6144d549fabe02befe91c84
stefan judis <stefanjudis@gmail.com>
2019-08-06 14:59:19 +0200

    update bash setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -24,18 +24,19 @@ SYNC_DOCUMENT_SID=$(twilio api:sync:v1:services:documents:create --unique-name=s
 echo "Created new sync document...";
 echo $SYNC_DOCUMENT_SID
 
-twilio api:sync:v1:services:documents:update --service-sid=$SYNC_SERVICE_SID --sid=$SYNC_DOCUMENT_SID --data="{ \"groupChatNumber\": \"$TWILIO_NUMBER\", participants: [{ \"name\": \"$MY_NAME\", \"number\": \"$MY_PHONE_NUMBER\" }]}"
+twilio api:sync:v1:services:documents:update --service-sid=$SYNC_SERVICE_SID --sid=$SYNC_DOCUMENT_SID --data="{ \"groupChatNumber\": \"$TWILIO_NUMBER\", participants: [{ \"name\": \"$MY_NAME\", \"number\": \"$MY_PHONE_NUMBER\" }]}" > /dev/null
 
-echo "Writing .env file"
 echo -e "TWILIO_NUMBER=$TWILIO_NUMBER\nSYNC_SERVICE_SID=$SYNC_SERVICE_SID\nSYNC_DOCUMENT_SID=$SYNC_DOCUMENT_SID" > .env
+echo "Wrote .env file"
 
 DEPLOY_OUTPUT=$(twilio serverless:deploy --force)
 SMS_ENDPOINT_URL=$(echo $DEPLOY_OUTPUT | grep -o "https://.*/sms/reply")
+echo "Deployed Twilio Runtime Function"
 echo $SMS_ENDPOINT_URL;
 
-twilio phone-numbers:update $TWILIO_NUMBER  --sms-url=$SMS_ENDPOINT_URL -o=json
-echo "Number updated with new function URL..."
+twilio phone-numbers:update $TWILIO_NUMBER  --sms-url=$SMS_ENDPOINT_URL -o=json > /dev/null
+echo "Updated number with new function URL..."
 
-twilio api:core:messages:create --from=$TWILIO_NUMBER --to=$MY_PHONE_NUMBER --body="Hello $MY_NAME. Happy to have you. Send '/help' to see all commands in your new SMS group chat."
+twilio api:core:messages:create --from=$TWILIO_NUMBER --to=$MY_PHONE_NUMBER --body="Hello $MY_NAME. Happy to have you. Send '/help' to see all commands in your new SMS group chat." -o=json > /dev/null
 
 echo "Your group SMS chat is ready!!!"
