fdbdb36a26fbf9d62bdbd2d4bf111705b919ac3d
stefan judis <stefanjudis@gmail.com>
2019-08-06 14:31:57 +0200

    Progress

---
+++ README.md
@@ -0,0 +1,4 @@
+git clone git@github.com:stefanjudis/twilio-serverless-sms-group-chat.git
+cd twilio-serverless-sms-group-chat
+npm install
+mv .env.sample .env
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,13 +6,19 @@ set -o allexport
 source .env
 set +o allexport
 
-if [ -z "$YOUR_NAME" ] || [ -z "$YOUR_PHONE_NUMBER" ]
+
+if [ -z "$MY_NAME" ] || [ -z "$MY_PHONE_NUMBER" ]
 then
   echo 'Missing variable... :/';
   echo 'Please run the setup script as follows:';
-  echo '$ YOUR_NAME=Jane YOUR_PHONE_NUMBER=+123... npm run setup';
+  echo '$ MY_NAME=Jane MY_PHONE_NUMBER=+123... npm run setup';
   exit 1;
 fi
+
+TWILIO_NUMBER=$(twilio api:core:available-phone-numbers:mobile:list --country-code DE -o json | jq -r '.[0] | .phoneNumber')
+twilio api:core:incoming-phone-numbers:local:create --phone-number="$TWILIO_NUMBER" -o json | jq -r '.[0] | .friendlyName' > /dev/null
+echo "Bought number..."
+echo $TWILIO_NUMBER
 
 SYNC_SERVICE_NAME="serverless-sms-group-chat";
 SYNC_SERVICE_SID=$(twilio api:sync:v1:services:create --friendly-name=$SYNC_SERVICE_NAME -o=json | jq '.[0].sid' -r);
@@ -24,18 +30,20 @@ SYNC_DOCUMENT_SID=$(twilio api:sync:v1:services:documents:create --unique-name=s
 echo "Created new sync document...";
 echo $SYNC_DOCUMENT_SID
 
-twilio api:sync:v1:services:documents:update --service-sid=$SYNC_SERVICE_SID --sid=$SYNC_DOCUMENT_SID --data="{ \"groupChatNumber\": \"$GROUP_SMS_NUMBER\", participants: [{ \"name\": \"$YOUR_NAME\", \"number\": \"$YOUR_PHONE_NUMBER\" }]}"
+twilio api:sync:v1:services:documents:update --service-sid=$SYNC_SERVICE_SID --sid=$SYNC_DOCUMENT_SID --data="{ \"groupChatNumber\": \"$TWILIO_NUMBER\", participants: [{ \"name\": \"$MY_NAME\", \"number\": \"$MY_PHONE_NUMBER\" }]}"
 
 echo "Writing .env file"
-echo -e "ACCOUNT_SID=$ACCOUNT_SID\nAUTH_TOKEN=$AUTH_TOKEN\nGROUP_SMS_NUMBER=$GROUP_SMS_NUMBER\nSYNC_SERVICE_SID=$SYNC_SERVICE_SID\nSYNC_DOCUMENT_SID=$SYNC_DOCUMENT_SID" > .env
+echo -e "ACCOUNT_SID=$ACCOUNT_SID\nAUTH_TOKEN=$AUTH_TOKEN\nTWILIO_NUMBER=$TWILIO_NUMBER\nSYNC_SERVICE_SID=$SYNC_SERVICE_SID\nSYNC_DOCUMENT_SID=$SYNC_DOCUMENT_SID" > .env
 
-DEPLOY_OUTPUT=$(npm run deploy)
+
+
+DEPLOY_OUTPUT=$(twilio serverless:deploy --force)
 SMS_ENDPOINT_URL=$(echo $DEPLOY_OUTPUT | grep -o "https://.*/sms/reply")
 echo $SMS_ENDPOINT_URL;
 
-twilio phone-numbers:update $GROUP_SMS_NUMBER  --sms-url=$SMS_ENDPOINT_URL -o=json
+twilio phone-numbers:update $TWILIO_NUMBER  --sms-url=$SMS_ENDPOINT_URL -o=json
 echo "Number updated with new function URL..."
 
-twilio api:core:messages:create --from=$GROUP_SMS_NUMBER --to=$YOUR_PHONE_NUMBER --body="Hello $YOUR_NAME. Happy to have you. Send '/help' to see all commands in your new SMS group chat."
+twilio api:core:messages:create --from=$TWILIO_NUMBER --to=$MY_PHONE_NUMBER --body="Hello $MY_NAME. Happy to have you. Send '/help' to see all commands in your new SMS group chat."
 
 echo "Your group SMS chat is ready!!!"
